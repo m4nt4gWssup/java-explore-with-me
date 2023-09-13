cff179f93cdad20a268dc1be088b82bb43fb713e
Петруха <petro.mandazhi@yandex.ru>
2023-09-13 03:13:39 +0300

    refactor: изменен код под поправки

--- ewm-service/src/main/java/ru/practicum/adminService/service/event/AdminEventsServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/adminService/service/event/AdminEventsServiceImpl.java
@@ -42,32 +42,9 @@ public class AdminEventsServiceImpl implements AdminEventsService {
         Event eventUpdate = EventMapper.toEvent(dto);
         Event event = eventRepository.findById(eventId)
                 .orElseThrow(() -> new NotFoundException(String.format("Событие с id = %s не найдено", eventId)));
-        if (dto.getDescription() != null) {
-            if (dto.getDescription().trim().isEmpty()
-                    || dto.getDescription().length() < 20
-                    || dto.getDescription().length() > 7000) {
-                throw new ValidationException("Описание не должно быть пустым и быть >20 и <7000 символов!");
-            } else {
-                event.setDescription(dto.getDescription());
-            }
-        }
-        if (dto.getAnnotation() != null) {
-            if (dto.getAnnotation().trim().isEmpty()
-                    || dto.getAnnotation().length() < 20
-                    || dto.getAnnotation().length() > 2000) {
-                throw new ValidationException("Аннотация не должна быть: \n" +
-                        "- пустой \n" +
-                        "- меньше 20 символов или больше 2000");
-            } else {
-                event.setAnnotation(dto.getAnnotation());
-            }
-        }
-        if (dto.getTitle() != null && !dto.getTitle().trim().isEmpty()) {
-            if (dto.getTitle().length() < 3 || dto.getTitle().length() > 120) {
-                throw new ValidationException("Не валидная длина заголовка!");
-            }
-            event.setTitle(dto.getTitle());
-        }
+        event.setDescription(dto.getDescription());
+        event.setAnnotation(dto.getAnnotation());
+        event.setTitle(dto.getTitle());
         if (event.getState().equals(State.PUBLISHED)) {
             throw new ConflictException("Невозможно опубликовать событие, так как оно находится " +
                     "в неправильном статусе: PUBLISHED");
--- ewm-service/src/main/java/ru/practicum/baseService/dto/event/UpdateEventRequest.java
+++ ewm-service/src/main/java/ru/practicum/baseService/dto/event/UpdateEventRequest.java
@@ -6,9 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import ru.practicum.baseService.dto.location.LocationDto;
-import ru.practicum.baseService.util.notblanknull.NotBlankNull;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 
@@ -19,11 +20,13 @@ import static ru.practicum.baseService.util.constants.Constants.DATE_FORMAT;
 @AllArgsConstructor
 public abstract class UpdateEventRequest {
     @Length(min = 20, max = 2000)
-    @NotBlankNull
+    @NotBlank
+    @NotNull
     private String annotation;
     private Long category;
     @Length(min = 20, max = 7000)
-    @NotBlankNull
+    @NotBlank
+    @NotNull
     private String description;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
     private LocalDateTime eventDate;
@@ -34,7 +37,8 @@ public abstract class UpdateEventRequest {
     private Long participantLimit;
     private Boolean requestModeration;
     @Length(min = 3, max = 120)
-    @NotBlankNull
+    @NotBlank
+    @NotNull
     private String title;
 
     @Override
